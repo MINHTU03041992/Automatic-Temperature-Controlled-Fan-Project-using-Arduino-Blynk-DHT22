7f87d8a5654a866788142e4e8fe8a8e70031a33d
MINHTU03041992 <45420683+MINHTU03041992@users.noreply.github.com>
2019-08-12 02:39:42 +0900

    Create dht22.ino

---
+++ dht22/dht22.ino
@@ -0,0 +1,103 @@
+#define BLYNK_PRINT Serial    
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+WidgetLED FAN(V0); 
+#include "DHT.h"
+#include <SimpleTimer.h>
+#define DHTPIN D7     // pin  DATA with D4
+#define FAN_PIN D2   // FAN RELAY
+#define DHTTYPE DHT22   
+#define COLD_TEMP     27
+#define HOT_TEMP      30
+#define TIME_FAN_ON  10
+#define READ_AIR_DATA_TM  1L  
+#define SEND_UP_DATA_TM   1L 
+#define AUTO_CTRL_TM      1L 
+char auth[] = "8555d69300fc404bbb402e49c5cb0189";
+char ssid[] = "106F3FF460AC";
+char pass[] = "vt20160930";
+float humDHT = 0;
+float tempDHT = 0;
+boolean fanStatus = 0;
+int timeFanOn = 1; 
+long sampleTimingSeconds = 20; 
+long startTiming = 0;
+long elapsedTime = 0;
+SimpleTimer timer;
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  pinMode(FAN_PIN, OUTPUT);
+ aplyCmd();
+  Serial.begin(115200);
+  dht.begin();    
+  Blynk.begin( auth, ssid , pass );
+  FAN.off();
+ startTimers();
+}
+void loop() {
+  timer.run(); 
+  Blynk.run();
+}
+BLYNK_WRITE(3) 
+{
+  int i = param.asInt();
+  if (i == 1)
+  {
+    fanStatus = !fanStatus;
+    aplyCmd();
+  }
+}
+
+void getDhtData(void)
+{
+tempDHT = dht.readTemperature();
+  humDHT = dht.readHumidity();
+  if (isnan(humDHT) || isnan(tempDHT))   
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+}
+void aplyCmd()
+{
+  if (fanStatus == 1)
+  {
+    Blynk.notify("fan ON");
+    digitalWrite(FAN_PIN, LOW);
+    FAN.on();
+  }
+
+  else {
+    digitalWrite(FAN_PIN, HIGH);
+    FAN.off();
+  }
+}
+void autoControlPlantation(void)
+
+{
+  if (tempDHT > HOT_TEMP)
+  {
+    turnFanOn();
+  }
+}
+
+void turnFanOn()
+{
+  fanStatus = 1;
+  aplyCmd();
+  delay (TIME_FAN_ON * 1000);
+  fanStatus = 0;
+  aplyCmd();
+}
+void startTimers(void)
+{
+  timer.setInterval(READ_AIR_DATA_TM * 1000, getDhtData);
+  timer.setInterval(SEND_UP_DATA_TM * 1000, sendUptime);
+  timer.setInterval(AUTO_CTRL_TM * 1000, autoControlPlantation);
+}
+void sendUptime()
+{
+  Blynk.virtualWrite(1, tempDHT); 
+  Blynk.virtualWrite(2, humDHT); 
+  
+}
